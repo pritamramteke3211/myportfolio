72e0e650128c6c5cc209d6486e98236ebd229977
Pritam Ramteke <pritamramteke3211@gmail.com>
2021-10-23 18:28:17 +0530

    6 commit

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u!&e=!x8zv6ng+23j=0d)v10ru@g$l*omvhhy36-qqd7cqyl=v'
+
+SECRET_KEY = os.environ['PORTFOLIO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
